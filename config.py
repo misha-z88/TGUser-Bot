2cd662f78ad3d30f4ad80f2f56183fce58cb4dd5
mikayilaz <148778675+mikayilaz@users.noreply.github.com>
2024-11-23 16:40:07 +0400

    Update config.py

--- config.py
+++ config.py
@@ -2,4 +2,5 @@
 API_ID = ""  # Замените на ваш API_ID
 API_HASH = ""  # Замените на ваш API_HASH
 OPENAI_API_KEY = ""  # Замените на ваш OpenAI ключ
+GEMINI_API_KEY = "" # Замените на ваш Gemini ключ
 WEATHER_API_KEY = ""  # Замените на ваш ключ API для погоды
